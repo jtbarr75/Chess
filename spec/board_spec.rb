f164cc161f7a5fcc5cf5afef17a42bfdf96bcab0
Jason Barr <Barr75@gmail.com>
2020-03-02 14:45:45 -0800

    add test for board background coloring

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,24 +3,9 @@ require './lib/board'
 describe Board do
   before(:each) { @board = Board.new }
 
-  W = "\u{25A0}"
-  B = "\u{25A1}"
-
-  describe "#create_blank_board" do
-    it "creates a 8x8 grid of alternating black and white squares" do
-      allow(@board).to receive(:grid).and_return(@board.create_blank_board)
-      expected = 
-      <<~EXPECTED
-      #{B} #{W} #{B} #{W} #{B} #{W} #{B} #{W}
-      #{W} #{B} #{W} #{B} #{W} #{B} #{W} #{B}
-      #{B} #{W} #{B} #{W} #{B} #{W} #{B} #{W}
-      #{W} #{B} #{W} #{B} #{W} #{B} #{W} #{B}
-      #{B} #{W} #{B} #{W} #{B} #{W} #{B} #{W}
-      #{W} #{B} #{W} #{B} #{W} #{B} #{W} #{B}
-      #{B} #{W} #{B} #{W} #{B} #{W} #{B} #{W}
-      #{W} #{B} #{W} #{B} #{W} #{B} #{W} #{B}
-      EXPECTED
-      expect { @board.print_grid }.to output(expected).to_stdout
+  describe "#colorize" do 
+    it "sets text backround color to white" do 
+      expect { puts @board.colorize("string", 'black', 'white') }.to output("\033[107;30mstring\033[0m\n").to_stdout
     end
   end
 end
