529420dcc22e09eadc59a7be3ca4a8ca120c56fa
Jason Barr <Barr75@gmail.com>
2020-03-06 16:12:19 -0800

    fix minor bug in move checking

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -3,7 +3,7 @@ require_relative 'piece.rb'
 class Knight < Piece
   def create_moves
     moves = []
-    [[2,1],[2,-1],[-2,1],[-2,1],[1,2],[-1,2],[1,-2],[-1,-2]].each do |move|
+    [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[-1,2],[1,-2],[-1,-2]].each do |move|
       next unless (row + move[0]).between?(0,7) && (col + move[1]).between?(0,7)
       target = board.at(row + move[0], col + move[1])
       if target.is_a? Piece
